aff505a8c9c559386874d171020c2d4790118570
Jakub Kolar <koljin@centrum.cz>
2016-01-23 23:10:14 +0100

    Happy new year!

--- src/main/java/com/github/jakubkolar/autobuilder/impl/BeanResolver.java
+++ src/main/java/com/github/jakubkolar/autobuilder/impl/BeanResolver.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2015 Jakub Kolar
+ * Copyright (c) 2016 Jakub Kolar
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
