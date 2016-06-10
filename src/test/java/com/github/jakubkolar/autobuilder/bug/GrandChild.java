1925e7ec0aa68d6667f57e75691bc24bae8374bd
Jakub Kolar <koljin@centrum.cz>
2016-06-10 14:13:50 +0200

    [AB-#27] Add a failing test

--- src/test/java/com/github/jakubkolar/autobuilder/bug/GrandChild.java
+++ src/test/java/com/github/jakubkolar/autobuilder/bug/GrandChild.java
@@ -24,29 +24,8 @@
 
 package com.github.jakubkolar.autobuilder.bug;
 
-import java.util.AbstractCollection;
-import java.util.AbstractSequentialList;
-import java.util.List;
-import java.util.Queue;
+public class GrandChild extends Child {
 
-public class SupertypeFields {
+    String grandChildField;
 
-    // Let's use java.util.LinkedList
-
-    // 1. Superclasses (direct & indirect)
-    AbstractSequentialList<?> abstractListField;
-    AbstractCollection<?> abstractCollectionField;
-    Object objectField;
-
-    // 2. Implemented interfaces
-    // a) direct
-    List<?> listField;
-    Cloneable cloneableField;
-    // b) indirect
-    Queue<?> queueField;
-    Iterable<?> iterableField;
-
-    // 3. Global config
-    Object globalOField;
-    Queue<?> globalQField;
 }
